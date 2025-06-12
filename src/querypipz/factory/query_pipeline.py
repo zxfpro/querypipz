bf926953e0ea0eb81351cc4f2231dffe0edfb6d8
823042332@qq.com <823042332@qq.com>
2025-06-12 14:18:45 +0800

    Auto build and publish

--- src/querypipz/factory/query_pipeline.py
+++ src/querypipz/factory/query_pipeline.py
@@ -5,43 +5,43 @@ from typing import List, Any
 from llama_index.core.query_engine.custom import CustomQueryEngine
 
 
-class PythonQueryEngine(CustomQueryEngine):
-    """RAG String Query Engine."""
+# class PythonQueryEngine(CustomQueryEngine):
+#     """RAG String Query Engine."""
 
-    retriever: BaseRetriever
-    response_synthesizer: BaseSynthesizer
-    llm: OpenAI
-    qa_prompt: PromptTemplate
-    stream:bool
+#     retriever: BaseRetriever
+#     response_synthesizer: BaseSynthesizer
+#     llm: OpenAI
+#     qa_prompt: PromptTemplate
+#     stream:bool
 
-    def custom_query(self, query_str: str):
-        # custom_query(query_str: str) -> STR_OR_RESPONSE_TYPE
-        # acustom_query(query_str: str) -> STR_OR_RESPONSE_TYPE
+#     def custom_query(self, query_str: str):
+#         # custom_query(query_str: str) -> STR_OR_RESPONSE_TYPE
+#         # acustom_query(query_str: str) -> STR_OR_RESPONSE_TYPE
 
-        queries = query_str.split('&')
-        nodes = []
-        for query_str in queries:
-            nodes += self.retriever.retrieve(query_str)
-        # nodes = self.retriever.retrieve(query_str)
-        print(query_str,'query_str')
-        context_str = "\n\n".join([n.node.get_metadata_str() for n in nodes])
-        if self.stream:
-            response = self.llm.stream_complete(
-                qa_prompt.format(safe_code=context_str, prompt=query_str)
-            )
+#         queries = query_str.split('&')
+#         nodes = []
+#         for query_str in queries:
+#             nodes += self.retriever.retrieve(query_str)
+#         # nodes = self.retriever.retrieve(query_str)
+#         print(query_str,'query_str')
+#         context_str = "\n\n".join([n.node.get_metadata_str() for n in nodes])
+#         if self.stream:
+#             response = self.llm.stream_complete(
+#                 qa_prompt.format(safe_code=context_str, prompt=query_str)
+#             )
 
-            def response_gen():
-                i = [0]
-                for r in response:
-                    i.append(len(str(r)))
-                    yield str(r)[i[-2]:]
+#             def response_gen():
+#                 i = [0]
+#                 for r in response:
+#                     i.append(len(str(r)))
+#                     yield str(r)[i[-2]:]
 
-            return StreamingResponse(response_gen=response_gen(),source_nodes=nodes)
-        else:
-            response = self.llm.complete(qa_prompt.format(safe_code=context_str, prompt=query_str))
+#             return StreamingResponse(response_gen=response_gen(),source_nodes=nodes)
+#         else:
+#             response = self.llm.complete(qa_prompt.format(safe_code=context_str, prompt=query_str))
     
-            response_obj = self.response_synthesizer.synthesize(response.text, nodes)
-            return Response(response=response.text,source_nodes=nodes) # TODO 增加metadata
+#             response_obj = self.response_synthesizer.synthesize(response.text, nodes)
+#             return Response(response=response.text,source_nodes=nodes) # TODO 增加metadata
 
 
 
@@ -65,11 +65,11 @@ class QueryPipeline:
 
             # instance = AnotherClass(param1=value1, param2=value2)
             pass
-            query_engine = RetrieverQueryEngine(
-                retriever=self.query.retriver,
-                response_synthesizer=response_synthesizer,
-                node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
-            )
+            # query_engine = RetrieverQueryEngine(
+            #     retriever=self.query.retriver,
+            #     response_synthesizer=response_synthesizer,
+            #     node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
+            # )
 
         else:
             raise Exception('Unknown type')
